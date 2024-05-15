008da333e4264973fd93d46e2e9748394707a8a4
Sofía García Barbés <UO263909@uniovi.es>
2024-05-16 00:22:29 +0200

    Eshopper - steps - dark/light modes

---
+++ js/navigation.js
@@ -0,0 +1,70 @@
+function t1_to_page_dark () {
+  window.location.replace('index_dark_t1.html')
+}
+function t1_to_page_light () {
+  window.location.replace('index_t1.html')
+}
+
+function page_to_t2_dark () {
+  window.location.replace('enunciado2_dark.html')
+}
+function page_to_t2_light () {
+  window.location.replace('enunciado2.html')
+}
+
+function t2_to_page_dark () {
+  window.location.replace('product-details3_dark_t2.html')
+}
+function t2_to_page_light () {
+  window.location.replace('product-details3_t2.html')
+}
+
+function page_to_t3_dark () {
+  window.location.replace('enunciado3_dark.html')
+}
+function page_to_t3_light () {
+  window.location.replace('enunciado3.html')
+}
+
+function t3_to_page_dark () {
+  window.location.replace('index_dark_t3.html')
+}
+function t3_to_page_light () {
+  window.location.replace('index_t3.html')
+}
+
+function page_to_t4_dark () {
+  window.location.replace('enunciado4_dark.html')
+}
+function page_to_t4_light () {
+  window.location.replace('enunciado4.html')
+}
+
+function t4_to_page_dark () {
+  window.location.replace('cart_dark_t4.html')
+}
+function t4_to_page_light () {
+  window.location.replace('cart_t4.html')
+}
+
+function page_to_t5_dark () {
+  window.location.replace('enunciado5_dark.html')
+}
+function page_to_t5_light () {
+  window.location.replace('enunciado5.html')
+}
+
+function t5_to_page_dark () {
+  window.location.replace('product-details3_dark_t5.html')
+}
+function t5_to_page_light () {
+  window.location.replace('product-details3_t5.html')
+}
+
+
+function page_to_end_dark () {
+    window.location.replace('final_eshopper_dark.html')
+  }
+  function page_to_end_light () {
+    window.location.replace('final_eshopper.html')
+  }
