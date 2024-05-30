51d3919bc2626bb32e8a7c5228e55f321ee86808
Sofía García Barbés <UO263909@uniovi.es>
2024-05-30 20:32:52 +0200

    QA navigation and UX

--- js/navigation.js
+++ js/navigation.js
@@ -1,70 +1,69 @@
 function t1_to_page_dark () {
-  window.location.replace('index_dark_t1.html')
+  window.location.href = ('index_dark_t1.html')
 }
 function t1_to_page_light () {
-  window.location.replace('index_t1.html')
+  window.location.href = ('index_t1.html')
 }
 
 function page_to_t2_dark () {
-  window.location.replace('enunciado2_dark.html')
+  window.location.href = ('enunciado2_dark.html')
 }
 function page_to_t2_light () {
-  window.location.replace('enunciado2.html')
+  window.location.href = ('enunciado2.html')
 }
 
 function t2_to_page_dark () {
-  window.location.replace('product-details3_dark_t2.html')
+  window.location.href = ('product-details3_dark_t2.html')
 }
 function t2_to_page_light () {
-  window.location.replace('product-details3_t2.html')
+  window.location.href = ('product-details3_t2.html')
 }
 
 function page_to_t3_dark () {
-  window.location.replace('enunciado3_dark.html')
+  window.location.href = ('enunciado3_dark.html')
 }
 function page_to_t3_light () {
-  window.location.replace('enunciado3.html')
+  window.location.href = ('enunciado3.html')
 }
 
 function t3_to_page_dark () {
-  window.location.replace('index_dark_t3.html')
+  window.location.href = ('index_dark_t3.html')
 }
 function t3_to_page_light () {
-  window.location.replace('index_t3.html')
+  window.location.href = ('index_t3.html')
 }
 
 function page_to_t4_dark () {
-  window.location.replace('enunciado4_dark.html')
+  window.location.href = ('enunciado4_dark.html')
 }
 function page_to_t4_light () {
-  window.location.replace('enunciado4.html')
+  window.location.href = ('enunciado4.html')
 }
 
 function t4_to_page_dark () {
-  window.location.replace('cart_dark_t4.html')
+  window.location.href = ('cart_dark_t4.html')
 }
 function t4_to_page_light () {
-  window.location.replace('cart_t4.html')
+  window.location.href = ('cart_t4.html')
 }
 
 function page_to_t5_dark () {
-  window.location.replace('enunciado5_dark.html')
+  window.location.href = ('enunciado5_dark.html')
 }
 function page_to_t5_light () {
-  window.location.replace('enunciado5.html')
+  window.location.href = ('enunciado5.html')
 }
 
 function t5_to_page_dark () {
-  window.location.replace('product-details3_dark_t5.html')
+  window.location.href = ('product-details3_dark_t5.html')
 }
 function t5_to_page_light () {
-  window.location.replace('product-details3_t5.html')
+  window.location.href = ('product-details3_t5.html')
 }
 
-
 function page_to_end_dark () {
-    window.location.replace('final_eshopper_dark.html')
-  }
-  function page_to_end_light () {
-    window.location.replace('final_eshopper.html')
-  }
+  window.location.href = ('final_eshopper_dark.html')
+}
+function page_to_end_light () {
+  window.location.href = ('final_eshopper.html')
+}
